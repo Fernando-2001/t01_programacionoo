c0306e8044b378ce7661274d02ab7f90ca04be57
jf7ch <jf7ch@DESKTOP-VBLNR4T>
2021-07-09 19:49:50 -0500

    tabla del 7

---
+++ src/diagramas/de/flujo/Main3.java
@@ -0,0 +1,17 @@
+package diagramas.de.flujo;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//imprimir la tabla del 7 multiplicado hasta el 12
+		
+		int n = 7;
+		for(int i=1; i>=12; i++) {
+			int m = 7*i;
+			System.out.println("7*"+i+"="+i);
+		}
+	}
+
+}
