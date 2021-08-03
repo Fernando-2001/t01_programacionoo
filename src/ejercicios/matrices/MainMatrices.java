0518031d646db33750624a12a8ab20c764afa8de
jf7ch <jf7ch@DESKTOP-VBLNR4T>
2021-08-02 20:09:44 -0500

    matrices y vectores

--- src/ejercicios/matrices/MainMatrices.java
+++ src/ejercicios/matrices/MainMatrices.java
@@ -4,7 +4,22 @@ public class MainMatrices {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//1. Declarar una matriz A[5][4] de tipo char.
+		char A[][]=new char [5][4];
+		
+		//2. Declarar una matriz B de tipo char con 5 filas y 6 columnas
+		char B[][]= new char[5][6];
+		
+		//3. Declarar una miMatriz de tipo long con 2 filas y 4 columnas
+		long miMatriz [][] = new long [2][4];
+		
+		//4. declarar una matriz miMatriz1 de long con 1 fila y 2 columnas.
+		// y asignar a la posición [0][0] el valor 2, posición [1][1] el valor 8 y en la posición [2][2] el valor 2. 
+		long miMatriz1 [][] = new long [1][2];
+		miMatriz [0][0] = 2;
+		miMatriz [1][1] = 8;
+		miMatriz [2][2] = 2;
+		
 	}
 
 }
