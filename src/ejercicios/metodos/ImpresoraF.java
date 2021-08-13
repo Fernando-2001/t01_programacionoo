d3bb1b9970fc17d5b9251c0502b8539e7e37ad61
jf7ch <jf7ch@DESKTOP-VBLNR4T>
2021-08-13 10:09:39 -0500

    Pablo Layedra.

---
+++ src/ejercicios/metodos/ImpresoraF.java
@@ -0,0 +1,20 @@
+package ejercicios.metodos;
+
+public class ImpresoraF {
+	
+	public String color;
+	
+	public ImpresoraF() {
+		
+	}
+	
+	public void imprimir(String datosFactura, int letra) {
+	System.out.println("Logo Ferreteria");
+	System.out.println("**************");
+	System.out.println(datosFactura);
+	System.out.println("**************");
+	System.out.println("Pie de firma");
+	
+	}
+
+}
